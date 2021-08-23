84329e0ea58fa2429e7a98d2df9ba7d43b975da5
pcyang <1083481902@qq.com>
2021-08-23 18:28:29 +0800

    加入启动自定义

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# young
+# young【<a href="http://youngjs.pcyang.cn" target="_blank">使用文档</a>】
 
 **基于 koa，使用 typescript，专注于高效开发后端接口的 nodejs 框架**
 
---
+++ config/lifecycle.ts
@@ -0,0 +1,10 @@
+import { Application } from "young-core";
+//启动自定义
+export default class appBoot {
+  before(app: Application) {
+    app.log.info("应用准备启动");
+  }
+  ready(app: Application) {
+    app.log.info("应用启动成功");
+  }
+}
