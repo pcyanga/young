9b2453f92b3ce7206c9f3962c28f0eeec8c0b1fc
奋斗2014 <1083481902@qq.com>
2022-03-30 16:33:16 +0800

    `

--- entity/api/photo.ts
+++
@@ -1,9 +0,0 @@
-import { Base } from "young-core";
-import { Entity, Column } from "typeorm";
-@Entity({ name: "photo" })
-export default class ApiPhoto extends Base {
-    @Column({
-        length: 1001,
-    })
-    name: String;
-}
