055ec2f016fbcd57f3394a1b72aa76ed1f8ebf4f
奋斗2014 <1083481902@qq.com>
2022-04-26 18:38:01 +0800

    支持socket自定义事件

--- socket/user.ts
+++ socket/user.ts
@@ -1,20 +1,23 @@
-import { youngSocket } from "young-core";
+import { socketEvent, youngSocket } from "young-core";
 import { Socket } from "socket.io";
 
 export default class userSocket extends youngSocket {
+  //选择性重写
   async connected(socket: Socket) {
     console.log("客户端链接", socket.id);
   }
+  //选择性重写
   async disconnect(socket: Socket) {
     console.log("客户端断开", socket.id);
   }
   /**
-   * 接收信息
+   * 接收信息 socketEvent 表示接受同方法名的事件，此处为接收 data 事件
    * @param msg 信息，多种类型
    * @param socket
    * @param io io对象，可用于广播
    * @returns 回复的信息，不回复就不return,或者return 空信息
    */
+  @socketEvent()
   async data(msg: any, socket: Socket, io) {
     console.log(new Date(), msg);
     return msg;
